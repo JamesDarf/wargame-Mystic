05d41f4d270b86cb4574aafdfc561d8088e825fd
Jay <lcm000522@gmail.com>
2025-01-22 14:41:18 +0900

    finish

---
+++ main/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, make_response
+
+app = Flask(__name__)
+
+# 플래그 파일 읽기
+with open('flag.txt', 'r') as file:
+    FLAG = file.read().strip()
+
+
+@app.route('/')
+def index():
+    # 쿠키에 flag 설정
+    response = make_response(render_template('index.html'))
+    response.set_cookie('flag', FLAG)  # 'flag' 쿠키에 플래그 값 설정
+    return response
+
+
+@app.route('/get-cookie')
+def get_cookie():
+    flag = request.cookies.get('flag', 'No Flag Found')
+    return f"{flag}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
